a05cfcd8e2a3be41f07e8678873d2676004f82ce
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-06 15:47:42 -0700

    Update random-teams.ts
    *wait why didn't I just*

--- data/mods/m4av6/random-teams.ts
+++ data/mods/m4av6/random-teams.ts
@@ -685,17 +685,13 @@ export class RandomTeams {
 					teamForcedPool.push('return');
 				}
 			}
-			console.log(teamForcedPool);
 			// and here's where we guarantee that one of them appears
 			if (moves.length < 4 && teamForcedPool.length) {
-				console.log("Trying to add from teamForcedPool");
-				const moveid = this.sampleNoReplace(teamForcedPool);
-				console.log(moveid);
+				const moveid = this.sample(teamForcedPool);
 				hasMove[moveid] = true;
 				moves.push(moveid);
 				continue;
 			}
-			console.log("Finished adding from teamForcedPool");
 
 			// Choose next 4 moves from learnset/viable moves and add them to moves list:
 			const pool = (movePool.length ? movePool : rejectedPool);
