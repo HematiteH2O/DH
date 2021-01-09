48ab1509115755aa1766be1ddad984ba0fe576e0
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-08 20:13:08 -0700

    Create pokedex.ts
    what would happen if I just

---
+++ data/mods/ondas/pokedex.ts
@@ -0,0 +1,22 @@
+export const Pokedex: {[speciesid: string]: SpeciesData} = {
+
+  lycanroc: {
+    num: 745,
+    name: "Lycanroc",
+    baseForme: "Midday",
+    types: ["Rock"],
+    baseStats: {hp: 75, atk: 115, def: 65, spa: 55, spd: 65, spe: 112},
+    abilities: {0: "No Ability"},
+    tactics: ["Intimidate", "Regenerator", "Unnerve", "Drought", "Leech Seed"],
+    heightm: 0.8,
+    weightkg: 25,
+    color: "Brown",
+    prevo: "Rockruff",
+    evoLevel: 25,
+    evoCondition: "during the day",
+    eggGroups: ["Field"],
+    otherFormes: ["Lycanroc-Midnight", "Lycanroc-Dusk"],
+    formeOrder: ["Lycanroc", "Lycanroc-Midnight", "Lycanroc-Dusk"],
+  },
+
+};
