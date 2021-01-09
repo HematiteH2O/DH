c1fb7e4c602ba07bd0305d567620d59814eede03
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-08 20:27:51 -0700

    Update conditions.ts
    oops

--- data/mods/ondas/conditions.ts
+++ data/mods/ondas/conditions.ts
@@ -1,45 +1,49 @@
 export const Conditions: {[k: string]: ConditionData} = {
 
 	intimidate: {
-		onStart(pokemon) {
-			let activated = false;
-			for (const target of pokemon.side.foe.active) {
-				if (!target || !this.isAdjacent(target, pokemon)) continue;
-				if (!activated) {
-					this.add('-ability', pokemon, 'Intimidate', 'boost');
-					activated = true;
-				}
-				if (target.volatiles['substitute']) {
-					this.add('-immune', target);
-				} else {
-					this.boost({atk: -1}, target, pokemon, null, true);
+		condition: {
+			onStart(pokemon) {
+				let activated = false;
+				for (const target of pokemon.side.foe.active) {
+					if (!target || !this.isAdjacent(target, pokemon)) continue;
+					if (!activated) {
+						this.add('-ability', pokemon, 'Intimidate', 'boost');
+						activated = true;
+					}
+					if (target.volatiles['substitute']) {
+						this.add('-immune', target);
+					} else {
+						this.boost({atk: -1}, target, pokemon, null, true);
+					}
 				}
 			}
 		},
-		name: "Intimidate",
 	},
 	unnerve: {
-		onPreStart(pokemon) {
-			this.add('-ability', pokemon, 'Unnerve', pokemon.side.foe);
+		condition: {
+			onPreStart(pokemon) {
+				this.add('-ability', pokemon, 'Unnerve', pokemon.side.foe);
+			},
+			onFoeTryEatItem: false,
 		},
-		onFoeTryEatItem: false,
-		name: "Unnerve",
 	},
 	drought: {
-		onStart(source) {
-			for (const action of this.queue) {
-				if (action.choice === 'runPrimal' && action.pokemon === source && source.species.id === 'groudon') return;
-				if (action.choice !== 'runSwitch' && action.choice !== 'runPrimal') break;
+		condition: {
+			onStart(source) {
+				for (const action of this.queue) {
+					if (action.choice === 'runPrimal' && action.pokemon === source && source.species.id === 'groudon') return;
+					if (action.choice !== 'runSwitch' && action.choice !== 'runPrimal') break;
+				}
+				this.field.setWeather('sunnyday');
 			}
-			this.field.setWeather('sunnyday');
 		},
-		name: "Drought",
 	},
 	regenerator: {
-		onSwitchOut(pokemon) {
-			pokemon.heal(pokemon.baseMaxhp / 3);
+		condition: {
+			onSwitchOut(pokemon) {
+				pokemon.heal(pokemon.baseMaxhp / 3);
+			},
 		},
-		name: "Regenerator",
 	},
 
 };
