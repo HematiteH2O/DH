5014bf5d1583cbe0a5bd8aaa9fd8273e733992c5
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-08 20:46:28 -0700

    Update scripts.ts

--- data/mods/ondas/scripts.ts
+++ data/mods/ondas/scripts.ts
@@ -1,10 +1,13 @@
 export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
-  onBeforeSwitchIn(pokemon) {
-    const tactics = pokemon.species.tactics && this.dex.getSpecies(pokemon.baseSpecies.tactics);
-    if (tactics) {
-      for (const tactic of tactics) {
-        pokemon.addVolatile(tactic);
-      }
-    }
-  },
+	onBeforeSwitchIn(pokemon) {
+		const tactics = pokemon.species.tactics && this.dex.getSpecies(pokemon.baseSpecies.tactics);
+		console.log(tactics);
+		if (tactics) {
+			for (const tactic of tactics) {
+				console.log(tactic);
+				pokemon.addVolatile(tactic);
+				console.log(pokemon.volatiles);
+			}
+		}
+	},
 };
