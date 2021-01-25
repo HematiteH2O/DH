f28a936ebe28fa1a2254d6d37b1c982b18417ea8
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-24 21:39:11 -0700

    Update rulesets.ts

--- data/mods/gen7dlcmons/rulesets.ts
+++ data/mods/gen7dlcmons/rulesets.ts
@@ -3,59 +3,8 @@ export const Formats: {[k: string]: FormatData} = {
 		effectType: 'Rule',
 		name: 'Data Mod',
 		desc: 'When a new Pokémon switches in for the first time, information about its types, stats and Abilities is displayed to both players.',
-		onSwitchIn(pokemon) {
-			if (pokemon.illusion) { // making sure Illusion isn't given away by this
-				const species = this.dex.getSpecies(pokemon.illusion.species.name);
-				const baseSpecies = Dex.getSpecies(pokemon.illusion.species.name);
-				let modded = false;
-				if (!baseSpecies) {
-					modded = true;
-				} else {
-					for (const type in [0, 1]) {
-						if (species.types[type] !== baseSpecies.types[type]) {
-							console.log(species.types[type] + " is different from " + baseSpecies.types[type]);
-							modded = true;
-						}
-					}
-					for (const stat in species.baseStats) {
-						if (species.baseStats.stat != baseSpecies.baseStats.stat) {
-							console.log(stat + ": " + species.baseStats.stat + " is different from " + baseSpecies.baseStats.stat);
-							modded = true;
-						}
-					}
-					for (const value in [0, 1, 'H', 'S']) {
-						if (species.abilities[value] != baseSpecies.abilities[value]) {
-							console.log(value + ": " + species.abilities[value] + " is different from " + baseSpecies.abilities[value]);
-							modded = true;
-						}
-					}
-				}
-				if (modded) {
-					console.log(species.name + " is different from base");
-				} else {
-					console.log(species.name + " is the same as in canon");
-					return;
-				}
-				console.log(species.name + " is being reported");
-				this.add('-start', pokemon, 'typechange', pokemon.illusion.getTypes(true).join('/'), '[silent]');
-				if (pokemon.illusion.switchedIn) return;
-				let abilities = species.abilities[0];
-				if (species.abilities[1]) {
-					abilities += ` / ${species.abilities[1]}`;
-				}
-				if (species.abilities['H']) {
-					abilities += ` / ${species.abilities['H']}`;
-				}
-				const baseStats = species.baseStats;
-				const type = species.types[0];
-				if (species.types[1]) {
-					const type2 = species.types[1];
-					this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"><img src="https://${Config.routes.client}/sprites/types/${type2}.png" alt="${type2}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-				} else {
-					this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-				}
-				pokemon.illusion.switchedIn = true;
-			} else {
+		onBegin() {
+			for (const pokemon of this.getAllPokemon()) {
 				const species = this.dex.getSpecies(pokemon.species.name);
 				const baseSpecies = Dex.getSpecies(pokemon.species.name);
 				let modded = false;
@@ -83,30 +32,43 @@ export const Formats: {[k: string]: FormatData} = {
 				}
 				if (modded) {
 					console.log(species.name + " is different from base");
+					pokemon.isModded = true;
 				} else {
 					console.log(species.name + " is the same as in canon");
 					return;
 				}
-				console.log(species.name + " is being reported");
-				this.add('-start', pokemon, 'typechange', pokemon.getTypes(true).join('/'), '[silent]');
-				if (!pokemon.switchedIn) {
-					let abilities = species.abilities[0];
-					if (species.abilities[1]) {
-						abilities += ` / ${species.abilities[1]}`;
-					}
-					if (species.abilities['H']) {
-						abilities += ` / ${species.abilities['H']}`;
-					}
-					const baseStats = species.baseStats;
-					const type = species.types[0];
-					if (species.types[1]) {
-						const type2 = species.types[1];
-						this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"><img src="https://${Config.routes.client}/sprites/types/${type2}.png" alt="${type2}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-					} else {
-						this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-					}
-					pokemon.switchedIn = true;
-				}
+			}
+		},
+		onSwitchIn(pokemon) {
+			let species = this.dex.getSpecies(pokemon.species.name);
+			let switchedIn = pokemon.switchedIn;
+			if (pokemon.illusion && pokemon.illusion.isModded) {
+				console.log(pokemon.illusion.name + " is being reported");
+				this.add('-start', pokemon, 'typechange', pokemon.illusion.getTypes(true).join('/'), '[silent]');
+				species = this.dex.getSpecies(pokemon.illusion.species.name);
+				switchedIn = pokemon.illusion.switchedIn;
+				if (switchedIn) return;
+				pokemon.illusion.switchedIn = true;
+			}
+			if (switchedIn) return;
+			pokemon.switchedIn = true;
+			let abilities = species.abilities[0];
+			if (species.abilities[1]) {
+				abilities += ` / ${species.abilities[1]}`;
+			}
+			if (species.abilities['H']) {
+				abilities += ` / ${species.abilities['H']}`;
+			}
+			if (species.abilities['S']) {
+				abilities += ` / ${species.abilities['S']}`;
+			}
+			const baseStats = species.baseStats;
+			const type = species.types[0];
+			if (species.types[1]) {
+				const type2 = species.types[1];
+				this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"><img src="https://${Config.routes.client}/sprites/types/${type2}.png" alt="${type2}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><br><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
+			} else {
+				this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities + `</span><span class="col abilitycol"></span></span><br><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
 			}
 		},
 /*
