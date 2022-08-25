c8fed8e522d95027c59f0aa1f242e0d1add6f32e
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-08-24 22:34:16 -0600

    Ditto, Zoroark for Terastal; spoiler move update

--- data/mods/svspeculative/scripts.ts
+++ data/mods/svspeculative/scripts.ts
@@ -7,6 +7,88 @@ export const Scripts: ModdedBattleScriptsData = {
 		customDoublesTiers: ['SV', 'SV (NFE)'],
 	},
 
+	// Pawmi
+
+	faintMessages(lastFirst = false) {
+		if (this.ended) return;
+		const length = this.faintQueue.length;
+		if (!length) return false;
+		if (lastFirst) {
+			this.faintQueue.unshift(this.faintQueue[this.faintQueue.length - 1]);
+			this.faintQueue.pop();
+		}
+		let faintData;
+		while (this.faintQueue.length) {
+			faintData = this.faintQueue.shift()!;
+			const pokemon: Pokemon = faintData.target;
+			if (!pokemon.fainted &&
+					this.runEvent('BeforeFaint', pokemon, faintData.source, faintData.effect)) {
+				this.add('faint', pokemon);
+				pokemon.side.pokemonLeft--;
+				this.runEvent('Faint', pokemon, faintData.source, faintData.effect);
+				this.singleEvent('End', pokemon.getAbility(), pokemon.abilityData, pokemon);
+				pokemon.clearVolatile(false);
+				pokemon.fainted = true;
+				pokemon.illusion = null;
+				pokemon.isActive = false;
+				pokemon.isStarted = false;
+				pokemon.side.faintedThisTurn = pokemon;
+				pokemon.side.mostRecentKO = pokemon; // only change
+			}
+		}
+
+		if (this.gen <= 1) {
+			// in gen 1, fainting skips the rest of the turn
+			// residuals don't exist in gen 1
+			this.queue.clear();
+		} else if (this.gen <= 3 && this.gameType === 'singles') {
+			// in gen 3 or earlier, fainting in singles skips to residuals
+			for (const pokemon of this.getAllActive()) {
+				if (this.gen <= 2) {
+					// in gen 2, fainting skips moves only
+					this.queue.cancelMove(pokemon);
+				} else {
+					// in gen 3, fainting skips all moves and switches
+					this.queue.cancelAction(pokemon);
+				}
+			}
+		}
+
+		let team1PokemonLeft = this.sides[0].pokemonLeft;
+		let team2PokemonLeft = this.sides[1].pokemonLeft;
+		const team3PokemonLeft = this.gameType === 'free-for-all' && this.sides[2]!.pokemonLeft;
+		const team4PokemonLeft = this.gameType === 'free-for-all' && this.sides[3]!.pokemonLeft;
+		if (this.gameType === 'multi') {
+			team1PokemonLeft = this.sides.reduce((total, side) => total + (side.n % 2 === 0 ? side.pokemonLeft : 0), 0);
+			team2PokemonLeft = this.sides.reduce((total, side) => total + (side.n % 2 === 1 ? side.pokemonLeft : 0), 0);
+		}
+		if (!team1PokemonLeft && !team2PokemonLeft && !team3PokemonLeft && !team4PokemonLeft) {
+			this.win(faintData && this.gen > 4 ? faintData.target.side : null);
+			return true;
+		}
+		if (!team2PokemonLeft && !team3PokemonLeft && !team4PokemonLeft) {
+			this.win(this.sides[0]);
+			return true;
+		}
+		if (!team1PokemonLeft && !team3PokemonLeft && !team4PokemonLeft) {
+			this.win(this.sides[1]);
+			return true;
+		}
+		if (!team1PokemonLeft && !team2PokemonLeft && !team4PokemonLeft) {
+			this.win(this.sides[2]);
+			return true;
+		}
+		if (!team1PokemonLeft && !team2PokemonLeft && !team3PokemonLeft) {
+			this.win(this.sides[3]);
+			return true;
+		}
+
+		if (faintData) {
+			this.runEvent('AfterFaint', faintData.target, faintData.source, faintData.effect, length);
+		}
+		return false;
+	},
+
 	// Terastal
 
 	canMegaEvo(pokemon) {
@@ -42,7 +124,7 @@ export const Scripts: ModdedBattleScriptsData = {
 		return true;
 	},
 	runSwitch(pokemon: Pokemon) { // modified for Terastal
-		if (pokemon.species.teraType) this.add('-start', pokemon, 'typechange', pokemon.species.types.join('/'), '[silent]');
+		if (pokemon.illusion ? pokemon.illusion.species.teraType : pokemon.species.teraType) this.add('-start', pokemon, 'typechange', pokemon.species.types.join('/'), '[silent]');
 		this.runEvent('Swap', pokemon);
 		this.runEvent('SwitchIn', pokemon);
 		if (this.gen <= 2 && !pokemon.side.faintedThisTurn && pokemon.draggedIn !== this.turn) {
@@ -65,6 +147,111 @@ export const Scripts: ModdedBattleScriptsData = {
 	},
 
 	pokemon: {
+
+		transformInto(pokemon: Pokemon, effect?: Effect) { // modded for Terastal
+			let unTeraSpecies = null;
+			if (pokemon.species.teraType) {
+				unTeraSpecies = this.battle.dex.deepClone(pokemon.species);
+				unTeraSpecies.teraType = null;
+				unTeraSpecies.types = pokemon.species.teraBoost;
+				unTeraSpecies.teraBoost = null;
+			}
+			const baseForm = pokemon.species.teraType ? unTeraSpecies : pokemon.species;
+			let teraSpecies = null;
+			if (this.species.teraType) {
+				teraSpecies = this.battle.dex.deepClone(baseForm);
+				teraSpecies.teraType = this.species.teraType;
+				teraSpecies.types = [teraSpecies.teraType];
+				teraSpecies.teraBoost = this.battle.dex.getSpecies(speciesId).types;
+			}
+			const rawSpecies = teraSpecies || baseForm;
+			if (pokemon.fainted || pokemon.illusion || (pokemon.volatiles['substitute'] && this.battle.gen >= 5) ||
+				 (pokemon.transformed && this.battle.gen >= 2) || (this.transformed && this.battle.gen >= 5) ||
+				 species.name === 'Eternatus-Eternamax') {
+				return false;
+			}
+
+			if (!this.setSpecies(species, effect, true)) return false;
+
+			this.transformed = true;
+			this.weighthg = pokemon.weighthg;
+
+			const types = pokemon.getTypes(true);
+			this.setType(pokemon.volatiles['roost'] ? pokemon.volatiles['roost'].typeWas : types, true);
+			this.addedType = pokemon.addedType;
+			this.knownType = this.side === pokemon.side && pokemon.knownType;
+			this.apparentType = pokemon.apparentType;
+
+			let statName: StatNameExceptHP;
+			for (statName in this.storedStats) {
+				this.storedStats[statName] = pokemon.storedStats[statName];
+			}
+			this.moveSlots = [];
+			this.set.ivs = (this.battle.gen >= 5 ? this.set.ivs : pokemon.set.ivs);
+			this.hpType = (this.battle.gen >= 5 ? this.hpType : pokemon.hpType);
+			this.hpPower = (this.battle.gen >= 5 ? this.hpPower : pokemon.hpPower);
+			for (const moveSlot of pokemon.moveSlots) {
+				let moveName = moveSlot.move;
+				if (moveSlot.id === 'hiddenpower') {
+					moveName = 'Hidden Power ' + this.hpType;
+				}
+				this.moveSlots.push({
+					move: moveName,
+					id: moveSlot.id,
+					pp: moveSlot.maxpp === 1 ? 1 : 5,
+					maxpp: this.battle.gen >= 5 ? (moveSlot.maxpp === 1 ? 1 : 5) : moveSlot.maxpp,
+					target: moveSlot.target,
+					disabled: false,
+					used: false,
+					virtual: true,
+				});
+			}
+			let boostName: BoostName;
+			for (boostName in pokemon.boosts) {
+				this.boosts[boostName] = pokemon.boosts[boostName]!;
+			}
+			if (this.battle.gen >= 6) {
+				const volatilesToCopy = ['focusenergy', 'gmaxchistrike', 'laserfocus'];
+				for (const volatile of volatilesToCopy) {
+					if (pokemon.volatiles[volatile]) {
+						this.addVolatile(volatile);
+						if (volatile === 'gmaxchistrike') this.volatiles[volatile].layers = pokemon.volatiles[volatile].layers;
+					} else {
+						this.removeVolatile(volatile);
+					}
+				}
+			}
+			if (effect) {
+				this.battle.add('-transform', this, pokemon, '[from] ' + effect.fullname);
+			} else {
+				this.battle.add('-transform', this, pokemon);
+			}
+			if (this.battle.gen > 2) this.setAbility(pokemon.ability, this, true);
+
+			// Change formes based on held items (for Transform)
+			// Only ever relevant in Generation 4 since Generation 3 didn't have item-based forme changes
+			if (this.battle.gen === 4) {
+				if (this.species.num === 487) {
+					// Giratina formes
+					if (this.species.name === 'Giratina' && this.item === 'griseousorb') {
+						this.formeChange('Giratina-Origin');
+					} else if (this.species.name === 'Giratina-Origin' && this.item !== 'griseousorb') {
+						this.formeChange('Giratina');
+					}
+				}
+				if (this.species.num === 493) {
+					// Arceus formes
+					const item = this.getItem();
+					const targetForme = (item?.onPlate ? 'Arceus-' + item.onPlate : 'Arceus');
+					if (this.species.name !== targetForme) {
+						this.formeChange(targetForme);
+					}
+				}
+			}
+
+			return true;
+		},
+
 		setType(newType: string | string[], enforce = false) { // modded for Terastal
 			// First type of Arceus, Silvally cannot be normally changed
 			if (!enforce) {
@@ -82,6 +269,7 @@ export const Scripts: ModdedBattleScriptsData = {
 
 			return true;
 		},
+
 		formeChange( // modded for Terastal
 		speciesId: string | Species, source: Effect = this.battle.effect,
 		 isPermanent?: boolean, message?: string
