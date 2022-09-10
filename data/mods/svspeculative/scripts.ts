5b2d07f40a806decaae57ba31a516ee8dd28f7b1
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-09-09 19:46:32 -0600

    Update scripts.ts

--- data/mods/svspeculative/scripts.ts
+++ data/mods/svspeculative/scripts.ts
@@ -28,14 +28,19 @@ export const Scripts: ModdedBattleScriptsData = {
 					} else {
 						let moveSource = null;
 						for (const source of learnset[moveid]) {
-							if (source.charAt(0) !== 8 && source.charAt(0) !== 7) continue;
-							if (source.charAt(1) === 'L') moveSource = ['8L1'];
-							if (source.charAt(1) === 'E' && moveSource !== ['8L1']) moveSource = ['8E'];
+							if (parseInt(moveSource.charAt(0)) < 7) continue; // only recent level/Egg moves
+							if (source.charAt(1) === 'L') {
+								moveSource = ['8L1'];
+								return; // prioritize level-up over Egg if something can be both
+							}
+							if (source.charAt(1) === 'E') moveSource = ['8E'];
 						}
 						if (moveSource) {
 							learnset[moveid] = moveSource;
+							console.log(moveid + moveSource);
 						} else {
 							delete learnset[moveid];
+							console.log(moveid + " deleted");
 						}
 					}
 				}
