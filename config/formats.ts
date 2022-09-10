469dc5acca633fc29db6d18bc8553ffe64165ca2
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-09-09 20:56:33 -0600

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -3318,8 +3318,7 @@ export const Formats: FormatList = [
 
 		searchShow: false,
 		
-		ruleset: ['Obtainable', '+Unobtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause', 'Sleep Clause Mod'],
-		// ruleset: ['Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause', 'Sleep Clause Mod'],
+		ruleset: ['Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause', 'Sleep Clause Mod'],
 		onValidateSet(set) {
 			const item = this.dex.getItem(set.item);
 			if (item.megaStone) return [`${set.name || set.species} is not allowed to Mega Evolve. (We have Terastal instead!)`];
