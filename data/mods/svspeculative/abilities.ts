197eb3b82b94a7a72b199c804a3a62e4a3ae1661
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-09-09 21:09:12 -0600

    Update abilities.ts

--- data/mods/svspeculative/abilities.ts
+++ data/mods/svspeculative/abilities.ts
@@ -21,7 +21,15 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 			if (!lastAttackedBy) return;
 			const damage = move.multihit ? move.totalDamage : lastAttackedBy.damage;
 			if (target.hp <= target.maxhp / 2 && target.hp + damage > target.maxhp / 2) {
-				this.boost({atk: 1, def: -1, spa: 1, spd: -1, spe: 1});
+				// this.boost({atk: 1, def: -1, spa: 1, spd: -1, spe: 1});
+				// the above works, but it takes forever! let's make it a shorter display:
+				const angerShellBoost: SparseBoostsTable = {};
+				angerShellBoost.def = -1;
+				angerShellBoost.spd = -1;
+				angerShellBoost.atk = 1;
+				angerShellBoost.spa = 1;
+				angerShellBoost.spe = 1;
+				this.boost(angerShellBoost);
 			}
 		},
 		name: "Anger Shell",
