0c730fef5e04701dd3374496ea39f09aade24c1d
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-10-17 17:17:33 -0600

    Create items.ts

---
+++ data/mods/nihilegohno/items.ts
@@ -0,0 +1,13 @@
+export const Items: {[itemid: string]: ModdedItemData} = {
+	nihilegiumz: {
+		name: "Nihilegium Z",
+		spritenum: 687,
+		onTakeItem: false,
+		// zMove: "Light That Burns the Sky",
+		// zMoveType: "Normal",
+		// (OR)
+		// zMoveFrom: "Photon Geyser",
+		num: -793,
+		gen: 7,
+	},
+};
