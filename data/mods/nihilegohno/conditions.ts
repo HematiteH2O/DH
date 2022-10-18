d02d491173926ee2d9e079c4238180a83549626c
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-10-17 23:14:07 -0600

    Update conditions.ts

--- data/mods/nihilegohno/conditions.ts
+++ data/mods/nihilegohno/conditions.ts
@@ -47,6 +47,7 @@ export const Conditions: {[k: string]: ConditionData} = {
 			if (attacker.moves.indexOf(move) >= attacker.fusionIndex) {
 				console.log(attacker.name);
 				attacker.name = attacker.hostName;
+				attacker.fullname = attacker.side.id + ': ' + attacker.name;
 				console.log(attacker.name);
 				if (attacker.species.id !== attacker.host.id) {
 					attacker.formeChange(attacker.host);
@@ -54,9 +55,8 @@ export const Conditions: {[k: string]: ConditionData} = {
 					if (!attacker.hasType('Poison') && attacker.addType('Poison')) this.add('-start', attacker, 'typeadd', 'Poison', '[silent]'); // three types!
 				}
 			} else {
-				console.log(attacker.name);
 				attacker.name = attacker.fusionName;
-				console.log(attacker.name);
+				attacker.fullname = attacker.side.id + ': ' + attacker.name;
 				if (attacker.species.id !== attacker.fusion.id) {
 					attacker.formeChange(attacker.fusion);
 					this.add('-start', attacker, 'typechange', attacker.getTypes(true).join('/'), '[silent]');
