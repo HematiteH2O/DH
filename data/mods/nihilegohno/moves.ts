188b0cbf197d3e68555a6342b8c97e68d1e357c2
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-10-17 18:44:35 -0600

    Create moves.ts

---
+++ data/mods/nihilegohno/moves.ts
@@ -0,0 +1,37 @@
+export const Moves: {[k: string]: ModdedMoveData} = {
+	uncannysynchronicity: {
+		num: -793,
+		accuracy: true,
+		basePower: 160,
+		category: "Special",
+		isNonstandard: "Past",
+		name: "Uncanny Synchronicity",
+		pp: 1,
+		priority: 0,
+		flags: {},
+		isZ: "nihilegiumz",
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Acid Downpour", target);
+		},
+		self: {
+			onHit(source) {
+				for (const pokemon of source.side.active) {
+					let statName = 'atk';
+					let bestStat = 0;
+					let s: StatNameExceptHP;
+					for (s in pokemon.storedStats) {
+						if (pokemon.storedStats[s] > bestStat) {
+							statName = s;
+							bestStat = pokemon.storedStats[s];
+						}
+					}
+					this.boost({statName: 1}, pokemon);
+				}
+			},
+		},
+		target: "allAdjacentFoes",
+		type: "Poison",
+		contestType: "Clever",
+	},
+};
