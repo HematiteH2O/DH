136447bb2faabc1492877a2a92b98508350bac5e
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-10-27 00:04:03 -0700

    please

--- data/mods/spookyhalloweenfuntimes/items.ts
+++ data/mods/spookyhalloweenfuntimes/items.ts
@@ -64,20 +64,190 @@ export const Items: {[itemid: string]: ModdedItemData} = {
 			pokemon.illusion = pokemon.side.pokemon[i];
 			pokemon.setAbility('illusion');
 		},
-		onDamagingHit(damage, target, source, move) {
-			if (target.illusion) {
-				target.useItem();
-				target.setAbility(target.baseAbility);
-			}
-		},
-		onFaint(pokemon) {
-			if (target.illusion) {
-				target.useItem();
-				target.setAbility(target.baseAbility);
-			}
-		},
+		// some of effect hard-coded into Illusion
 		num: -4,
 		gen: 8,
 		desc: "The holder's Ability is Illusion. Consumed when the Illusion is broken.",
+	},
+	bloonketcostume: {
+		name: "Bloonket Costume",
+		spritenum: 385,
+		fling: {
+			basePower: 10,
+		},
+		onStart(pokemon) {
+			if (!pokemon.illusion) {
+				const details = pokemon.species.name + (pokemon.level === 100 ? '' : ', L' + pokemon.level) +
+					(pokemon.gender === '' ? '' : ', ' + pokemon.gender) + (pokemon.set.shiny ? ', shiny' : '');
+				this.add('replace', pokemon, details);
+			}
+		},
+		// some of effect hard-coded into Illusion and some into Team Preview rule
+		desc: "At team preview, the holder is disguised as Bloonket!",
+		num: -5,
+		gen: 8,
+	},
+	ghostsheet: {
+		name: "Ghost Sheet",
+		spritenum: 385,
+		fling: {
+			basePower: 10,
+		},
+		onStart(pokemon) {
+			if (!pokemon.getTypes().join() === 'Ghost' && pokemon.setType('Ghost')) {
+				this.add('-start', pokemon, 'typechange', 'Ghost');
+			}
+		},
+		onEnd(pokemon) {
+			if (!pokemon.getTypes().join() === pokemon.baseSpecies.types.join && pokemon.setType(pokemon.baseSpecies.types.join)) {
+				this.add('-end', pokemon, 'typechange', '[silent]');
+			}
+		},
+		desc: "The holder's type is pure Ghost.",
+		num: -6,
+		gen: 8,
+	},
+	trickortreatbag: {
+		name: "Trick-or-Treat Bag",
+		spritenum: 385,
+		fling: {
+			basePower: 10,
+		},
+		onStart(pokemon) {
+			let activated = 0;
+			this.add('-message', `${pokemon.illusion ? pokemon.illusion.name : pokemon.name}: Boo!`);
+			for (const target of pokemon.side.foe.active) {
+				if (!target || target.fainted) continue;
+				for (const moveSlot of ((pokemon.illusion && pokemon.illusion.moveSlots) ? pokemon.illusion.moveSlots : pokemon.moveSlots)) {
+					const move = this.dex.getMove(moveSlot.move);
+					if (move.category === 'Status') continue;
+					const moveType = move.id === 'hiddenpower' ? target.hpType : move.type;
+					if (
+						this.dex.getImmunity(moveType, target) && this.dex.getEffectiveness(moveType, target) > 0
+					) {
+						this.add('-message', `${target.illusion ? target.illusion.name : target.name}: Eeeek!`);
+						activated ++;
+						continue;
+					}
+				}
+			}
+			this.add('-message', `...`);
+			if (activated) {
+				this.add('-message', `${pokemon.illusion ? pokemon.illusion.name : pokemon.name} got some candy!`);
+				this.heal(pokemon.baseMaxhp * 0.25 * activated);
+			} else {
+				this.add('-message', `It wasn't that scary, so nothing happened.`);
+			}
+		},
+		desc: "On entry, the holder scares the target to restore HP.",
+		num: -7,
+		gen: 8,
+	},
+	nightgoggles: {
+		name: "Night Goggles",
+		spritenum: 604,
+		fling: {
+			basePower: 80,
+		},
+		onStart(pokemon) {
+			if (pokemon.volatiles['choicelock']) {
+				this.debug('removing choicelock: ' + pokemon.volatiles['choicelock']);
+			}
+			pokemon.removeVolatile('choicelock');
+		},
+		onModifyMove(move, pokemon) {
+			pokemon.addVolatile('choicelock');
+		},
+		onImmunity(type, pokemon) {
+			if (type === 'Ghost') return false;
+		},
+		isChoice: true,
+		desc: "The holder is immune to Ghost but can only use the first move it selects.",
+		num: -8,
+		gen: 8,
+	},
+	eucalyptuspatch: {
+		name: "Eucalyptus Patch",
+		spritenum: 292,
+		fling: {
+			basePower: 10,
+		},
+		onModifyMove(move) {
+			if (!move || !move.flags['contact'] || move.target === 'self' || move.type !== 'Grass') return;
+			if (!move.secondaries) {
+				move.secondaries = [];
+			}
+			move.secondaries.push({
+				chance: 100,
+				volatileStatus: 'yawn',
+			});
+		},
+		onAnyHit(target, source, move) {
+			if (source && source !== target && source === this.effectData.target && move && move.flags['contact'] && move.type === 'Grass' && source.useItem()) {
+				target.addVolatile('yawn');
+			}
+		},
+		desc: "Inflicts drowsiness when using a Grass-type contact move (consumed).",
+		num: -9,
+		gen: 8,
+	},
+	crocs: {
+		name: "Crocs",
+		spritenum: 715,
+		fling: {
+			basePower: 10,
+		},
+		desc: "Makes the holder twice as vulnerable to hazards.",
+		num: -10,
+		gen: 8,
+		// implemented in moves.ts
+	},
+	swapcauldron: {
+		name: "Swap Cauldron",
+		spritenum: 715,
+		fling: {
+			basePower: 80,
+		},
+		onStart(pokemon) {
+			const possibleTargets = pokemon.side.foe.active.filter(foeActive => foeActive && this.isAdjacent(pokemon, foeActive));
+			let target = null;
+			if (possibleTargets.length) {
+				let rand = 0;
+				if (possibleTargets.length > 1) rand = this.random(possibleTargets.length);
+				const target = possibleTargets[rand];
+			}
+			if (!target) return;
+			const yourItem = target.takeItem(pokemon);
+			const myItem = pokemon.takeItem();
+			if (target.item || pokemon.item || (!yourItem && !myItem)) {
+				if (yourItem) target.item = yourItem.id;
+				if (myItem) pokemon.item = myItem.id;
+				return false;
+			}
+			if (
+				(myItem && !this.singleEvent('TakeItem', myItem, pokemon.itemData, target, pokemon, this.effectData, myItem)) ||
+				(yourItem && !this.singleEvent('TakeItem', yourItem, target.itemData, pokemon, target, this.effectData, yourItem))
+			) {
+				if (yourItem) target.item = yourItem.id;
+				if (myItem) pokemon.item = myItem.id;
+				return false;
+			}
+			this.add('-item', pokemon, 'Swap Cauldron');
+			if (myItem) {
+				target.setItem(myItem);
+				this.add('-item', target, myItem, '[from] move: Trick');
+			} else {
+				this.add('-enditem', target, yourItem, '[silent]', '[from] move: Trick');
+			}
+			if (yourItem) {
+				pokemon.setItem(yourItem);
+				this.add('-item', pokemon, yourItem, '[from] move: Trick');
+			} else {
+				this.add('-enditem', pokemon, myItem, '[silent]', '[from] move: Trick');
+			}
+		},
+		desc: "Swaps with the target's item on entry!",
+		num: -11,
+		gen: 8,
 	},
 };
