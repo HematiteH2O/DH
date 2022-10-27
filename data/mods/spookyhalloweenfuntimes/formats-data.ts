8f210785f4dfe06cfe16808bc6d41495fbafecc0
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-10-27 00:46:39 -0700

    Starting

---
+++ data/mods/spookyhalloweenfuntimes/formats-data.ts
@@ -0,0 +1,8 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+
+	zubat: { tier: "Spooky (NFE)" },
+	golbat: { tier: "Spooky (NFE)" },
+	crobat: { tier: "Spooky" },
+	groabat: { tier: "Spooky" },
+
+};
