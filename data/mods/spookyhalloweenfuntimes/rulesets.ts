95f99eca50d05c0fd21165e2edd101685d7cc840
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-10-27 20:54:36 -0700

    Update rulesets.ts

--- data/mods/spookyhalloweenfuntimes/rulesets.ts
+++ data/mods/spookyhalloweenfuntimes/rulesets.ts
@@ -66,15 +66,15 @@ export const Formats: {[k: string]: FormatData} = {
 				if (pokemon.switchedIn) return;
 				pokemon.switchedIn = true;
 			}
-			let abilities = species.abilities[0];
+			let abilities = this.dex.getAbility(species.abilities[0]).name;
 			if (species.abilities[1]) {
-				abilities += ` / ${species.abilities[1]}`;
+				abilities += ` / ${this.dex.getAbility(species.abilities[1]).name}`;
 			}
 			if (species.abilities['H']) {
-				abilities += ` / ${species.abilities['H']}`;
+				abilities += ` / ${this.dex.getAbility(species.abilities['H']).name}`;
 			}
 			if (species.abilities['S']) {
-				abilities += ` / ${species.abilities['S']}`;
+				abilities += ` / ${this.dex.getAbility(species.abilities['S']).name}`;
 			}
 			const baseStats = species.baseStats;
 			const type = species.types[0];
@@ -150,21 +150,6 @@ export const Formats: {[k: string]: FormatData} = {
 					}
 				}
 			}
-		},
-		onAfterMega(pokemon) {
-			this.add('-start', pokemon, 'typechange', pokemon.getTypes(true).join('/'), '[silent]');
-			const species = this.dex.getSpecies(pokemon.species.name);
-			const abilities = species.abilities;
-			const baseStats = species.baseStats;
-			const type = species.types[0];
-			if (species.types[1]) {
-				const type2 = species.types[1];
-				this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${type}.png" alt="${type}" height="14" width="32"><img src="http://play.pokemonshowdown.com/sprites/types/${type2}.png" alt="${type2}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + this.dex.getAbility(abilities[0]).name + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-			} else {
-				this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="http://play.pokemonshowdown.com/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + this.dex.getAbility(abilities[0]).name + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-			}
-			if (species.creator) this.hint(`${species.name} was submitted by ${species.creator}!`);
-			pokemon.switchedIn = true;
 		},
 	},
 };
