2250b91ccff900d9d151b12fd5a3b18db65b3d42
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-12-15 23:59:45 -0800

    Update scripts.ts

--- data/mods/dexsheet/scripts.ts
+++ data/mods/dexsheet/scripts.ts
@@ -2,7 +2,11 @@ export const Scripts: ModdedBattleScriptsData = {
 	init() {
 		for (const id in this.dataCache.Pokedex) {
 			const poke = this.dataCache.Pokedex[id];
-			let abilities = this.dataCache.Abilities[this.toID(poke.abilities[0])].name;
+			if (!poke || !poke.abilities || !poke.baseStats || !poke.types) return;
+			let abilities = ``;
+			if (poke.abilities[0]) {
+				abilities += `${this.dataCache.Abilities[this.toID(poke.abilities[0])].name}`;
+			}
 			if (poke.abilities[1]) {
 				abilities += ` / ${this.dataCache.Abilities[this.toID(poke.abilities[1])].name}`;
 			}
@@ -13,7 +17,7 @@ export const Scripts: ModdedBattleScriptsData = {
 				abilities += ` // (${this.dataCache.Abilities[this.toID(poke.abilities['S'])].name})`;
 			}
 			const sheetOutput: string[] = [];
-			sheetOutput.push(`=IMAGE((poke.evos ? "nfe" : "fe") + "~https://www.smogon.com/forums//media/minisprites/` + poke.name.toLowerCase() + `.png",3)~` + poke.name + "~" + poke.types[0] + "~" + (poke.types[1] ? poke.types[1] : "") + "~" + poke.baseStats.hp + "~" + poke.baseStats.atk + "~" + poke.baseStats.def + "~" + poke.baseStats.spa + "~" + poke.baseStats.spd + "~" + poke.baseStats.spe + "~~~~~~~" + abilities);
+			sheetOutput.push(`=IMAGE((poke.evos ? "nfe" : "fe") + "~https://www.smogon.com/forums//media/minisprites/` + poke.name.toLowerCase() + `.png",3)~` + poke.name + "~" + poke.types[0] + "~" + (poke.types[1] ? poke.types[1] : "") + "~" + poke.baseStats.hp + "~" + poke.baseStats.atk + "~" + poke.baseStats.def + "~" + poke.baseStats.spa + "~" + poke.baseStats.spd + "~" + poke.baseStats.spe + "~~~~~~~~" + abilities);
 			poke.sheetOutput = sheetOutput;
 		}
 	},
