b9f975edcc6ecedc2852296b3a3a09101c02b735
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2022-12-18 18:34:46 -0700

    Continuing to be a super genius

--- data/mods/variantscompare/scripts.ts
+++ data/mods/variantscompare/scripts.ts
@@ -34,7 +34,7 @@ export const Scripts: ModdedBattleScriptsData = {
 						}
 					}
 					if (learnset2[moveid]) { // if it learns the move itself
-						for (const source of learnset[moveid]) {
+						for (const source of learnset2[moveid]) {
 							if (parseInt(source.charAt(0)) >= pokeGen && parseInt(source.charAt(1)) !== 'V') oriLearned = true;
 						}
 					}
