e7b64cac14589ed0c8b48eaf8147de4ba4bc5378
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-07-08 06:19:18 -0600

    don't stay up all night

--- data/mods/kalos/scripts.ts
+++ data/mods/kalos/scripts.ts
@@ -202,7 +202,7 @@ export const Scripts: ModdedBattleScriptsData = {
 				for (const level in poke.learnsetCumulative.learnset) {
 					if (poke.learnsetCumulative.learnset[level].length) {
 						poke.learnsetCumulative.learnset[level].sort;
-						for (const moveid in poke.learnsetCumulative.learnset(level)) {
+						for (const moveid in poke.learnsetCumulative.learnset[level]) {
 							sheetOutput += `\n` + level + ` - ` + move;
 						}
 					}
