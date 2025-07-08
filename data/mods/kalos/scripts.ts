f66c8604dddfff7b7b984d9a2b4d276d0b839716
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-07-08 02:13:12 -0600

    Update scripts.ts

--- data/mods/kalos/scripts.ts
+++ data/mods/kalos/scripts.ts
@@ -6,39 +6,6 @@ export const Scripts: ModdedBattleScriptsData = {
 			// 'round', 'gigaimpact', 'swagger',
 		];
 
-		const movepoolSections = {
-			SpeedControl: [
-				'tailwind', 'trickroom', 'stickyweb', 'bulldoze', 'cottonspore', 'stringshot', 'electroweb', 'icywind', 'thunderwave', 'nuzzle', 'lowsweep', 'mudshot', 'rocktomb',
-				'scaryface',
-			],
-			Priority: [
-				'extremespeed', 'fakeout', 'feint', 'aquajet', 'bulletpunch', 'iceshard', 'machpunch', 'quickattack', 'shadowsneak', 'suckerpunch', 'vacuumwave', 'watershuriken',
-			],
-			PriorityPrankster: [
-				'assist', 'copycat', 'mefirst', 'metronome', 'mirrormove', 'naturepower',
-			],
-			Spread: [
-				'acid', 'aircutter', 'blizzard', 'bubble', 'dazzlinggleam', 'disarmingvoice', 'electroweb', 'eruption', 'heatwave', 'hypervoice', 'icywind', 'incinerate', 'muddywater',
-				'powdersnow', 'razorleaf', 'razorwind', 'rockslide', 'snarl', 'strugglebug', 'swift', 'twister', 'waterspout', 'boomburst', 'bulldoze', 'discharge', 'earthquake',
-				'explosion', 'lavaplume', 'magnitude', 'paraboliccharge', 'petalblizzard', 'selfdestruct', 'sludgewave', 'surf', 'synchronoise',
-			],
-			FieldEffect: [
-				'raindance', 'sunnyday', 'sandstorm', 'hail', 'electricterrain', 'grassyterrain', 'trickroom', 'gravity', 'perishsong', 'spikes', 'stealthrock', 'toxicspikes', 'stickyweb',
-				'poisongas',
-			],
-			DamageMitigation: [
-				'reflect', 'lightscreen', 'quickguard', 'wideguard', 'matblock', 'fakeout', 'followme', 'ragepowder', 'captivate', 'growl', 'snarl', 'strugglebug', 'babydolleyes', 'charm',
-				'eerieimpulse', 'featherdance', 'kingsshield', 'memento', 'nobleroar', 'partingshot', 'tickle', 'mysticalfire', 'willowisp', 'watersport', 'mudsport', 'grasswhistle',
-				'hypnosis', 'lovelykiss', 'sing', 'sleeppowder', 'spore', 'yawn',
-			],
-			Other: [
-				'taunt', 'torment', 'encore', 'disable', 'skydrop', 'destinybond', 'counter', 'mirrorcoat', 'endeavor', 'metalburst', 'superfang', 'leer', 'screech', 'faketears',
-				'acidspray', 'metalsound', 'allyswitch', 'healpulse', 'helpinghand', 'skillswap', 'entrainment', 'simplebeam', 'soak', 'uturn', 'voltswitch', 'batonpass', 'partingshot',
-				'acupressure', 'dragondance', 'flamecharge', 'quiverdance', 'shellsmash', 'shiftgear', 'swordsdance', 'bellydrum', 'poweruppunch', 'chargebeam', 'fierydance', 'growth',
-				'nastyplot', 'tailglow', 'rototiller', 'flowershield', 'magneticflux',
-			],
-		};
-
 		let printno = 0;
 		for (const id in this.dataCache.Pokedex) {
 			const poke = this.dataCache.Pokedex[id];
@@ -47,13 +14,20 @@ export const Scripts: ModdedBattleScriptsData = {
 			if (this.dataCache.Learnsets[id] && this.dataCache.Learnsets[id].learnset) {
 				printno++;
 				poke.learnsetCumulative = {};
-				for (const section in movepoolSections) {
-					poke.learnsetCumulative[section] = {
-						Moves: [],
-					};
-				}
 				// start with the vanilla learnset
 				const learnset = this.modData('Learnsets', this.toID(id)).learnset;
+				let learnset2 = null;
+				let learnset3 = null;
+
+				// if the Pokémon has pre-evolutions, add their learnsets, too!
+				if (poke.prevo) {
+					const poke2 = this.dataCache.Pokedex[this.toID(poke.prevo)];
+					learnset2 = this.modData('Learnsets', this.toID(poke.prevo)).learnset;
+					if (poke2.prevo) {
+						learnset3 = this.modData('Learnsets', this.toID(poke2.prevo)).learnset;
+					}
+				}
+
 				for (const moveid in this.dataCache.Moves) {
 
 // NOTICE: you will need to account for pre-evolutions' movepools before generating new level-up lists!
@@ -83,40 +57,104 @@ export const Scripts: ModdedBattleScriptsData = {
 							}
 						}
 					}
-					if (learned && !learnedLvUp && !learnedTm && learnedOras) learnedLvUp = true; // add tutors and Egg moves to level-up, but not event moves
-					if (learned) {
-						let title: string[] = [move.name];
-						for (const section in movepoolSections) {
-							if (movepoolSections[section].includes(moveid)) {
-								poke.learnsetCumulative[section].Moves.push(title);
+					if (learnset2 && learnset2[moveid]) { // if it learns the move
+						for (const source of learnset2[moveid]) {
+							if (parseInt(source.charAt(0)) === 6) {
+								learned = true;
+								if (source.charAt(1) === 'L') learnedLvUp = true;
+								if (source.charAt(1) === 'M' && !postgameTms.includes(moveid)) learnedTm = true;
+								if (source.charAt(1) === 'E' || source.charAt(1) === 'T') learnedOras = true;
 							}
 						}
 					}
+					if (learnset3 && learnset3[moveid]) { // if it learns the move
+						for (const source of learnset3[moveid]) {
+							if (parseInt(source.charAt(0)) === 6) {
+								learned = true;
+								if (source.charAt(1) === 'L') learnedLvUp = true;
+								if (source.charAt(1) === 'M' && !postgameTms.includes(moveid)) learnedTm = true;
+								if (source.charAt(1) === 'E' || source.charAt(1) === 'T') learnedOras = true;
+							}
+						}
+					}
+					if (learned && !learnedLvUp && !learnedTm && learnedOras) learnedLvUp = true; // add tutors and Egg moves to level-up, but not event moves
+					if (learnedLvUp) poke.learnsetCumulative.Moves.push(moveid);
 				}
+				for (const moveid in poke.learnsetCumulative.Moves) {
+					let lv = 1;
+					const move = this.dataCache.Moves[moveid];
+					let basePower = 1;
+					if (move.basePower && move.basePower > 0) {
+						basePower = move.basePower;
+						if (['fellstinger'].includes(moveid)) basePower = 30;
+						if (['flyingpress'].includes(moveid)) basePower = 80;
+						if (['leechlife'].includes(moveid)) basePower = 20;
+						if (['mysticalfire'].includes(moveid)) basePower = 65;
+						if (['paraboliccharge'].includes(moveid)) basePower = 50;
+						if (['suckerpunch'].includes(moveid)) basePower = 80;
+						if (['tackle'].includes(moveid)) basePower = 50;
+						if (['lusterpurge'].includes(moveid)) basePower = 70;
+						if (['mistball'].includes(moveid)) basePower = 70;
+						if (move.multihit) {
+							if (move.multihit === 2) basePower *= 2;
+							else if (move.multihit === 3) basePower *= 6; // Triple Kick, Triple Axel
+							else basePower *= 3;
+						}
+						if (move.flags && move.flags['charge']) basePower *= 0.75;
+						if (move.target && ['allAdjacentFoes', 'allAdjacent'].includes(move.target)) basePower *= 1.1;
+						if (move.recoil) {
+							if (move.recoil === [1, 4]) basePower *= 8 / 9;
+							if (move.recoil === [33, 100]) basePower *= 6 / 7;
+							if (move.recoil === [1, 2]) basePower *= 4 / 5;
+						}
+						if (move.type && (move.type === 'Normal' || !poke.types.includes[move.type])) basePower *= 0.8;
+						if (move.willCrit) basePower *= 1.5;
+					}
+
+					lv = basePower - 40;
+					lv *= 0.8;
+					lv = Math.floor(lv);
+					if (lv < 1) lv = 1;
+
+					// coverage type lenience
+					if (['Fire', 'Flying', 'Rock'].includes(moveid) && 9 < lv && lv < 14) lv = 9; // Viola
+					if (['Water', 'Grass', 'Fighting', 'Ground', 'Steel'].includes(moveid) && 24 < lv && lv < 30) lv = 24; // Grant
+					if (['Flying', 'Psychic', 'Fairy'].includes(moveid) && 32 < lv && lv < 38) lv = 32; // Korrina
+					if (['Fire', 'Ice', 'Poison', 'Flying', 'Bug'].includes(moveid) && 35 < lv && lv < 41) lv = 35; // Ramos
+					if (['Fire', 'Fighting', 'Ground'].includes(moveid) && 39 < lv && lv < 45) lv = 39; // Aliana and Clemont
+					if (['Poison', 'Steel'].includes(moveid) && 44 < lv && lv < 50) lv = 44; // Valerie
+					if (['Bug', 'Ghost', 'Dark'].includes(moveid) && 51 < lv && lv < 57) lv = 51; // Olympia
+					if (['Psychic', 'Ground'].includes(moveid) && 51 < lv && lv < 57) lv = 51; // Xerosic
+					if (['Flying', 'Psychic', 'Fairy'].includes(moveid) && 55 < lv && lv < 61) lv = 55; // Tierno??
+					if (['Fire', 'Fighting', 'Rock', 'Steel'].includes(moveid) && 63 < lv && lv < 69) lv = 63; // Wulfric
+					if (72 < lv) lv = 72; // c'mon you can learn anything you want at that point
+
+					// assign exceptional levels to (some) status moves
+					// if (['fellstinger'].includes(moveid)) lv = 1;
+
+					// evolutions learn exclusive moves at their evolution level, if possible!
+					if (poke.evoLevel) {
+						if ((!learnset2 || !learnset2[moveid]) && (!learnset3 || !learnset3[moveid])) {
+							if (lv < evoLevel) lv = evoLevel;
+						}
+					}
+
+					// first assign the move a level
+
+					// then send it to the learnset
+					let movelv: string[] = [`\n ` + lv + ` - ` + move.name];
+					poke.learnsetCumulative.learnset.push(movelv);
+				}
+				poke.learnsetCumulative.learnset.sort();
 
 				// abilities
-				if (!poke || !poke.num || !poke.abilities || !poke.types || !poke.baseStats) return;
-				let abilities = ``;
-				if (poke.abilities[0]) abilities += `${this.dataCache.Abilities[this.toID(poke.abilities[0])].name}`;
-				if (poke.abilities[1]) abilities += ` / ${this.dataCache.Abilities[this.toID(poke.abilities[1])].name}`;
-				if (poke.abilities['H']) abilities += ` // ${this.dataCache.Abilities[this.toID(poke.abilities['H'])].name}`;
-				if (poke.abilities['S']) abilities += ` // (${this.dataCache.Abilities[this.toID(poke.abilities['S'])].name})`;
-				// icon name
-				var iconid = id;
-				// var iconid = iconname.replace(" ", `-`).replace(`.`, ``).replace(`:`, ``).replace(`\u2019`, ``); // to get rid of spaces and periods
-
+				if (!poke || !poke.learnsetCumulative.learnset) return;
 				// finalize sheetOutput now.........
 				let sheetOutput: string[] = [
-					(printno) + `~3~=IMAGE("https://www.smogon.com/forums//media/minisprites/` + iconid + `.png",3)~` + poke.name + "~" + poke.types[0] + "~" + (poke.types[1] ? poke.types[1] : "") + "~" + abilities + "~" + poke.baseStats.hp + "~" + poke.baseStats.atk + "~" + poke.baseStats.def + "~" + poke.baseStats.spa + "~" + poke.baseStats.spd + "~" + poke.baseStats.spe + "~"
+					`\n\n` + poke.name + `\n`
 				];
-				for (const section in movepoolSections) {
-					if (
-						poke.learnsetCumulative[section].Moves.length
-					) {
-						sheetOutput += "~" + poke.learnsetCumulative[section].Moves.sort();
-					} else {
-						sheetOutput += "~"; // just a blank cell
-					}
+				for (const move in poke.learnsetCumulative.learnset) {
+					sheetOutput += move;
 				}
 				poke.sheetOutput = sheetOutput;
 			}
