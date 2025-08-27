d90ace08c9abee73ecd874cce7031de5e53ece12
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-08-27 01:06:09 -0600

    Update scripts.ts

--- data/mods/randomizer/scripts.ts
+++ data/mods/randomizer/scripts.ts
@@ -206,6 +206,10 @@ Other post-Gen V moves I probably *can* backport if it comes up
 					}
 					if (move.category && move.category === 'Status') moveName = `0` + moveName; // attacks should be the last move learned at a level so NPCs don't often get stuck with none
 					if (move.num && move.num > 559) moveName = moveName + ` *`; // identify post-Gen V moves
+					if (levelLearned > 101) {
+						console.log(poke.name + ` - ` + moveName + ` - ` + levelLearned);
+						continue;
+					}
 					poke.learnsetCumulative.learnset[levelLearned].movesLearned.push(moveName);
 				}
 
