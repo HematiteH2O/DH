bd5808680ef4a46f61353863f89de67155caa1cc
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-09-02 15:12:07 -0600

    Update scripts.ts

--- data/mods/randomizer/scripts.ts
+++ data/mods/randomizer/scripts.ts
@@ -263,7 +263,9 @@ Other post-Gen V moves I probably *can* backport if it comes up
 					}
 
 					// skip identical combinations for now
-					for (const combo of chosenCombinations) if (chosenCombinations[combo] === [type2, type1]) continue;
+					for (const combo in chosenCombinations) {
+						if (chosenCombinations[combo][type1] === type2 && chosenCombinations[combo][type2] === type1) continue;
+					}
 
 					// score for defensive matchups
 					let defScore = 0;
