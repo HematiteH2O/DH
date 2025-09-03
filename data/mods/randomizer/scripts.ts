a38c42d86b3f28620ab649fb35f757a7af612277
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2025-09-02 22:33:54 -0600

    okay it worked--

--- data/mods/randomizer/scripts.ts
+++ data/mods/randomizer/scripts.ts
@@ -667,14 +667,12 @@ export const Scripts: ModdedBattleScriptsData = {
 					type2: [poke.types[1] ? poke.types[1] : poke.types[0]],
 				};
 			}
-			if (poke.name === "Sandslash-Alola") console.log(poke.chosenType.score + poke.chosenType.type1 + ((poke.chosenType.type2 !== poke.chosenType.type1) ? `/` + poke.chosenType.type2 : ` `));
 			if ((poke.types[0] === poke.chosenType.type2) || (poke.types[1] && poke.types[1] === poke.chosenType.type1)) {
 				const secondType = poke.chosenType.type1;
 				const firstType = poke.chosenType.type2;
 				poke.chosenType.type1 = firstType;
 				poke.chosenType.type2 = secondType;
 			}
-			if (poke.name === "Sandslash-Alola") console.log(poke.chosenType.score + poke.chosenType.type1 + ((poke.chosenType.type2 !== poke.chosenType.type1) ? `/` + poke.chosenType.type2 : ` `));
 
 			// console.logging
 			let samples: string[] = [poke.name + ` samples: `];
